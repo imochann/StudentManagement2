624841d235b22a6dd313e427b7d9c221f7ffaf9e
imochan <skerlet28@gmail.com>
2025-09-02 07:14:36 +0900

    javaスタンダード第14回演習課題の提出

--- src/main/java/raisetech/student/management/controller/StudentController.java
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -1,6 +1,8 @@
 package raisetech.student.management.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.student.management.controller.converter.StudentConverter;
@@ -13,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
+@Controller
 public class StudentController {
 
     private StudentService service;
@@ -26,11 +28,13 @@ public class StudentController {
     }
 
     @GetMapping("/studentList")
-    public List<StudentDetail> getStudentList() {
+    public String getStudentList(Model model) {
         List<Student> students = service.searchStudentList();
         List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
 
-        return converter.convertStudentDetails(students, studentsCourses);
+        model.addAttribute("studentList", converter.convertStudentDetails(students, studentsCourses));
+
+        return "studentList";
     }
 
 
